cc9eeb72c8a7d53b8598f0ace1b8f76fcca62f20
Christian Enevoldsen <chrene91@gmail.com>
2015-01-28 16:25:10 +0100

    Adds framework projects

---
+++ PureLayout/PureLayoutOSX/PureLayoutOSX.h
@@ -0,0 +1,19 @@
+//
+//  PureLayoutOSX.h
+//  PureLayoutOSX
+//
+//  Created by ChristianEnevoldsen on 28/01/15.
+//  Copyright (c) 2015 Reversebox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PureLayoutOSX.
+FOUNDATION_EXPORT double PureLayoutOSXVersionNumber;
+
+//! Project version string for PureLayoutOSX.
+FOUNDATION_EXPORT const unsigned char PureLayoutOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PureLayoutOSX/PublicHeader.h>
+
+
