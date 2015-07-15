8f15eecbc9ed21bbf8451074a0cd17d194c3970e
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2015-07-15 19:01:33 +0200

    Made conversion dependent on current architecture
    Fixed `Implicit conversion loses floating point precision: 'double' to 'CGFloat' (aka. 'float')` warning.

--- PureLayout/PureLayout/PureLayout+Internal.h
+++ PureLayout/PureLayout/PureLayout+Internal.h
@@ -29,7 +29,7 @@
 
 /** A constant that represents the smallest valid positive value for the multiplier of a constraint,
     since a value of 0 will cause the second item to be lost in the internal auto layout engine. */
-static const CGFloat kMULTIPLIER_MIN_VALUE = 0.00001; // very small floating point numbers (e.g. CGFLOAT_MIN) can cause problems
+static const CGFLOAT_TYPE kMULTIPLIER_MIN_VALUE = (CGFLOAT_TYPE)0.00001; // very small floating point numbers (e.g. CGFLOAT_MIN) can cause problems
 
 
 /**
