5d999efc2b296429d9e537eef313a3d98832d998
Denis Gladyshev <d.gladyshev@lamoda.ru>
2019-11-15 17:08:04 +0300

    add swift package manager support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "PureLayout",
+    platforms: [.iOS(.v8), .macOS(.v10_10), .tvOS(.v9)],
+    products: [
+        .library(
+            name: "PureLayout",
+            targets: ["PureLayout"])
+    ],
+    targets: [
+        .target(
+            name: "PureLayout",
+            path: "PureLayout/PureLayout")
+    ]
+)
