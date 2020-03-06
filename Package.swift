e28277628b5ac88a273f84bced166d26622c6d29
Masato Mizuta <mst.mizuta@gmail.com>
2020-03-05 21:11:24 -0500

    Fix swiftpm again

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .target(
             name: "PureLayout",
             path: "PureLayout/PureLayout",
-            publicHeadersPath: "PureLayout/PureLayout")
+            publicHeadersPath: ".")
     ]
 )
