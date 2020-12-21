0aaa9879a7d3bcf21f11be9dc8da5e62e102c8b3
Anton Dolzhenko <dolzhenko.tony@gmail.com>
2020-12-21 10:26:03 +0200

    increase iOS deployment target to 9.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "PureLayout",
-    platforms: [.iOS(.v8), .macOS(.v10_10), .tvOS(.v9)],
+    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9)],
     products: [
         .library(
             name: "PureLayout",
